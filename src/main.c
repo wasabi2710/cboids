e63a354f7c5da0e4b70c0025653766ac8849bab8
wasabi2710 <em.vannin.2001@gmail.com>
2025-07-01 03:50:21 -0400

    done bird shape

--- src/main.c
+++ src/main.c
@@ -3,48 +3,56 @@
 #include <stdio.h>
 
 typedef struct {
-    Vector2 startPos;
-    Vector2 endPos;
-} Line;
+    Vector2 startPos, endPos; //vectors for center masses
+    Vector2 cPos, dPos;       //perpendicular points
+} Bird;
 
-//todo
-//reapply center mass rotation first
-//apply rotation around y axis => mpos target rotation
 int main() {
-    InitWindow(1000,500,"CBOIDS");
+    InitWindow(1000, 500, "CBOIDS");
     SetTargetFPS(60);
 
-    Line line = {.startPos={400, 20}, .endPos={400, 200}};
+    Bird bird = { .startPos = {0, 0}, .endPos = {400, 200} };
 
-    Vector2 mpos = {0};
+    while (!WindowShouldClose()) {
+        float dt = GetFrameTime();
+        Vector2 mousePos = GetMousePosition();
 
-    while(!WindowShouldClose()) {
+        float dx = mousePos.x - bird.endPos.x;
+        float dy = mousePos.y - bird.endPos.y;
+        float distance = sqrtf(dx*dx + dy*dy);
+
+        if (distance > 0.1f) {
+            float uv_x = dx / distance;
+            float uv_y = dy / distance; 
+
+            bird.endPos.x += uv_x * 20 * dt;
+            bird.endPos.y += uv_y * 20 * dt;
+
+            float rad = atan2f(dy, dx);
+            float lineLength = 50.0f;
+            bird.startPos.x = bird.endPos.x + lineLength * cosf(rad);
+            bird.startPos.y = bird.endPos.y + lineLength * sinf(rad);
+        }
+
+        float ux = bird.startPos.x - bird.endPos.x;
+        float uy = bird.startPos.y - bird.endPos.y;
+        float len = sqrtf(ux*ux + uy*uy);
+
+        if (len > 0.001f) {
+            float scale = 10.0f / len;
+            bird.cPos.x = bird.endPos.x - uy * scale;
+            bird.cPos.y = bird.endPos.y + ux * scale;
+
+            bird.dPos.x = bird.endPos.x + uy * scale;
+            bird.dPos.y = bird.endPos.y - ux * scale;
+        }
+
         BeginDrawing();
         ClearBackground(BLACK);
 
-        mpos = GetMousePosition();
-        
-        //y axis rotation
-        //apply rotation matrix
-        //for a given radian
-        //=> x' = x * cos0 - x * sin0
-        //=> y' = y * sin0 + y * cos0        
-        float rad = 3.14 / 4;    
-
-        float x = line.startPos.x;
-        float y = line.startPos.y;
-
-        float xrad = x * cosf(rad) - y * sinf(rad);
-        float yrad = x * sinf(rad) + y * cosf(rad);
-
-        //finalize rendering
-        DrawLine(line.startPos.x, line.startPos.y, line.endPos.x, line.endPos.y, RAYWHITE);
-        DrawLine(xrad, yrad, line.endPos.x, line.endPos.y, RAYWHITE);
-
-        //debug 
-        char log[100];
-        snprintf(log, sizeof(log), "MPOS.X: %f, MPOS.Y: %f\n", mpos.x, mpos.y);
-        DrawText(log,20,20,20,GREEN);
+        DrawLineV(bird.startPos, bird.cPos, RAYWHITE);
+        DrawLineV(bird.startPos, bird.dPos, RAYWHITE);
+        DrawLineV(bird.cPos, bird.dPos, RAYWHITE);
 
         EndDrawing();
     }
